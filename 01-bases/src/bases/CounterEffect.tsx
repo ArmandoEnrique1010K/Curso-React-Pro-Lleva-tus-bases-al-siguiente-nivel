d5b8731358a9aa17a1a7d323ed113bf16fc5b49a
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-09-28 15:35:54 -0500

    19

--- 01-bases/src/bases/CounterEffect.tsx
+++ 01-bases/src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
 const MAXIMUN_COUNT = 10;
@@ -6,6 +6,7 @@ const MAXIMUN_COUNT = 10;
 export const CounterEffect = () => {
 
     const [counter, setCounter] = useState(5);
+    const counterElement = useRef<HTMLHeadingElement>(null);
 
     const handleClick = () => {
         setCounter(prev => Math.min(prev + 1, MAXIMUN_COUNT));
@@ -17,9 +18,9 @@ export const CounterEffect = () => {
 
         console.log('%c Se llego al valor máximo', 'color: red; background-color: black;');
 
-        gsap.to('h2', { y: -10, duration: 10, ease: 'ease.out' }).then(() => {
-            gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' })
-        })
+        const tl = gsap.timeline();
+        tl.to(counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' })
+            .to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' })
     }, [counter]);
 
     return (
@@ -28,7 +29,7 @@ export const CounterEffect = () => {
                 CounterEffect: {counter}
             </h1>
 
-            <h2>{counter}</h2>
+            <h2 ref={counterElement}>{counter}</h2>
 
             <button onClick={handleClick}>+1</button>
             <hr />
---
+++ 01-bases/src/bases/CounterHook.tsx
@@ -0,0 +1,22 @@
+import { useCounter } from "../hooks/useCounter"
+
+
+export const CounterHook = () => {
+
+    const { counter, elementToAnimate, handleClick } = useCounter({
+        maxCount: 15
+    })
+
+    return (
+        <>
+            <h1>
+                CounterHook: {counter}
+            </h1>
+
+            <h2 ref={elementToAnimate}>{counter}</h2>
+
+            <button onClick={handleClick}>+1</button>
+            <hr />
+        </>
+    )
+}

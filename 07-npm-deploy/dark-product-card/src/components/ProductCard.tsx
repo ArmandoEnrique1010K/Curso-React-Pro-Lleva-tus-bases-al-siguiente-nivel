e8e4e484d21fdeb3d8e4be6f79f4f903528ab667
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-11-09 20:04:06 -0500

    114

---
+++ 07-npm-deploy/dark-product-card/src/components/ProductCard.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useProduct } from '../hooks/useProduct'
+import { createContext } from 'react';
+import type { ProductContextProps, Product, OnChangeArgs, InitialValues, ProductCardHandlers } from '../interfaces/interfaces';
+import styles from '../styles/styles.module.css';
+import type { JSX } from 'react/jsx-runtime';
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext
+
+export interface Props {
+    children: (args: ProductCardHandlers) => JSX.Element;
+    product: Product;
+    className?: string;
+    style?: React.CSSProperties;
+    onChange?: (args: OnChangeArgs) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
+
+    const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProduct({
+        onChange,
+        product,
+        value,
+        initialValues,
+    });
+
+    return (
+        <Provider value={{
+            counter, increaseBy, product, maxCount
+        }}>
+            <div
+                className={` ${styles.productCard} ${className}`}
+                style={style}
+            >
+                {children({
+                    count: counter,
+                    isMaxCountReached,
+                    maxCount: initialValues?.maxCount,
+                    product,
+                    increaseBy,
+                    reset
+                })}
+            </div>
+
+        </Provider>
+    )
+}
---
+++ 07-npm-deploy/dark-product-card/src/hooks/useProduct.ts
@@ -0,0 +1,55 @@
+import { useEffect, useRef, useState } from "react"
+import type { InitialValues, OnChangeArgs, Product } from "../interfaces/interfaces";
+
+export interface UseProductProps {
+    product: Product;
+    onChange?: (args: OnChangeArgs) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+export const useProduct = ({ onChange, product, value = 0, initialValues }: UseProductProps) => {
+
+    const [counter, setCounter] = useState<number>(initialValues?.count || value)
+    console.log(initialValues?.count)
+
+    const isMounted = useRef(false);
+
+    const increaseBy = (value: number) => {
+        let newValue = Math.max(counter + value, 0);
+
+        if (initialValues?.maxCount) {
+            newValue = Math.min(newValue, initialValues.maxCount)
+        }
+
+        setCounter(newValue)
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        onChange && onChange({
+            product: product,
+            count: newValue
+        })
+    }
+
+    const reset = () => {
+        setCounter(initialValues?.count || value)
+    }
+
+    useEffect(() => {
+        if (!isMounted.current) return;
+
+        setCounter(value)
+    }, [value])
+
+    useEffect(() => {
+        isMounted.current = true;
+    }, [])
+
+    return {
+        counter,
+        isMaxCountReached: !!initialValues?.count && initialValues.maxCount === counter,
+        maxCount: initialValues?.maxCount,
+        increaseBy,
+        reset
+    }
+}
